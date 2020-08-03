c9af75e486022a2d6aa1a313f6fa396fecb23a50
David Sumner <davidphilipsumner@gmail.com>
2020-08-03 22:32:14 +0100

    typo fixes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-    has_many :comments, dependent: destroy;
+    has_many :comments, dependent: :destroy;
 
     # Validation of POST requests made to this model
     validates :title, presence: true, length: {minimum:5};
